5cb589fd655c53ca479efc6514feaba14af3cbe0
Max <contact@mstoiber.com>
2015-11-30 17:47:39 +0100

    A11y improvements of the generated buttons, slight copy change
    Closes #11

--- js/components/Footer.react.js
+++ js/components/Footer.react.js
@@ -6,11 +6,11 @@ var Footer = React.createClass({
 				<div className="grid-row">
 					<div className="grid-50">
 						<p>Love or loathe them, sharing buttons will not go away for a while. The default social media sharing scripts provided by the networks themselves are big, often download multiple files and track users across the web.</p>
-						<p>This generator outputs social media sharing buttons that <em>do not use JavaScript</em>, which means they <em>load incredibly fast</em> (they only use a single HTTP request), <em>don't block your website from rendering</em> and <em>do not track the user</em>. (Also, they look nice)</p>
+						<p>This generator outputs social media sharing buttons that <em>do not use JavaScript</em>, which means they <em>load incredibly fast</em> (they only use a single HTTP request), <em>don't block your website from rendering</em>, are <em>accessible</em> and <em>don't track the user</em>. (Also, they look nice)</p>
 					</div>
 					<div className="grid-50">
 						<p>Using these buttons is as easy as copying the HTML code into your <code>.html</code> files, and the CSS code into your <code>.css</code> file.</p>
-						<p>The buttons are sized using <code>em</code> units, so to change the size you just have to set the <code>font-size</code> property of the <code>.resp-sharing-button</code> class.</p>
+						<p>The buttons are sized using <code>em</code> units, so the only step to change the size of the buttons is to set the <code>font-size</code> property of the <code>.resp-sharing-button</code> class.</p>
 					</div>
 				</div>
 				<div className="grid-row">
--- js/components/PreviewButton.react.js
+++ js/components/PreviewButton.react.js
@@ -12,10 +12,10 @@ var PreviewButton = React.createClass({
 		switch(this.props.size) {
 			case "small":
 				shareText = "";
-				icon.push(<div key={"preview-button-" + name} className="resp-sharing-button__icon" dangerouslySetInnerHTML={ this._makeIcon(this.props.icon) }></div>);
+				icon.push(<div aria-hidden="true" key={"preview-button-" + name} className="resp-sharing-button__icon" dangerouslySetInnerHTML={ this._makeIcon(this.props.icon) }></div>);
 				break;
 			case "large":
-				icon.push(<div key={"preview-button-" + name} className="resp-sharing-button__icon" dangerouslySetInnerHTML={ this._makeIcon(this.props.icon) }></div>);
+				icon.push(<div aria-hidden="true" key={"preview-button-" + name} className="resp-sharing-button__icon" dangerouslySetInnerHTML={ this._makeIcon(this.props.icon) }></div>);
 			case "medium":
 				shareText = "Share on " + name;
 				break;
@@ -26,7 +26,8 @@ var PreviewButton = React.createClass({
 			<a className="resp-sharing-button__link"
 			   key={ name + "share-link" }
 			   href={ network.link }
-			   target="_blank">
+			   target="_blank"
+				 aria-label={ shareText }>
 				<div key={ name + "share-button"} className={ "resp-sharing-button resp-sharing-button--" + id.toLowerCase() + " resp-sharing-button--" + this.props.size}>
 					{ icon }
 					{ shareText }
